dc6bf364c86fa3af07247dcc800a6cc7525c65e8
Tino Matsika <43498809+Matsika@users.noreply.github.com>
2019-04-05 22:49:55 -0230

    Update CornerDetection.java

--- assignment5/CornerDetection.java
+++ assignment5/CornerDetection.java
@@ -219,4 +219,48 @@ public class CornerDetection extends Frame implements ActionListener {
             return x2_y2_xy;
             
         }
+	
+	void dog(){
+            int l, t, r, b, dx, dy;
+            Color clr1, clr2;
+            int gray1, gray2;
+            for(int q =0; q < height;q++){
+                t = q==0 ? q : q-1;
+		b = q==height-1 ? q : q+1;
+                for(int p=0; p < width; p++){
+                    l = p==0 ? p : p-1;
+                    r = p==width-1 ? p : p+1;
+                    clr1 = new Color(source.image.getRGB(l,q));
+                    clr2 = new Color(source.image.getRGB(r,q));
+                    gray1 = clr1.getRed() + clr1.getGreen() + clr1.getBlue();
+                    gray2 = clr2.getRed() + clr2.getGreen() + clr2.getBlue();
+                    dx = (gray2 - gray1) / 3;
+                    clr1 = new Color(source.image.getRGB(p,t));
+                    clr2 = new Color(source.image.getRGB(p,b));
+                    gray1 = clr1.getRed() + clr1.getGreen() + clr1.getBlue();
+                    gray2 = clr2.getRed() + clr2.getGreen() + clr2.getBlue();
+                    dy = (gray2 - gray1) / 3;
+                    dx = Math.max(-128, Math.min(dx, 127));
+                    dy = Math.max(-128, Math.min(dy, 127));
+                    ix2[p][q] = dx * dx;
+                    iy2[p][q] = dy * dy;
+                    ixy[p][q] = dx * dy;
+                    int red,green,blue;
+                    red = ix2[p][q];
+                    green = iy2[p][q];
+                    blue = ixy[p][q];
+                    if(red > 255){red = 255;}
+                    if(red < 0) {red = 0;}
+                    if(green > 255){green = 255;}
+                    if(green < 0){green = 0;}
+                    if(blue > 255){blue = 255;}
+                    if(blue < 0){blue = 0;}
+                    target.image.setRGB(p,q,new Color(red,green,blue).getRGB());
+                    
+                }
+                
+            }
+            target.repaint();
+            
+        }
 }
